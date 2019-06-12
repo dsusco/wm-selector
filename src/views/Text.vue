185f713aae081b07ff802bf2ad165b3ca916ad99
David Susco <dsusco@clarku.edu>
2019-06-12 10:26:12 -0400

    added text view and tests

--- src/views/Text.vue
+++ src/views/Text.vue
@@ -1,14 +1,55 @@
 <template>
   <main id="main" class="text-view">
-    Text
+    <pre><template v-if="label">{{label}}
+</template>{{armyList}}, {{pointsCost}} points
+{{version}}
+--------------------------------
+<template v-for="unit in units">{{padLeft(unit.pointsCost)}} - {{unit.number}} {{unit.id}}<template v-for="upgrade in unitUpgrades(unit.id)">
+{{padLeft(' ')}} - {{upgrade.number}} {{upgrade.id}} ({{upgrade.pointsCost}})</template>
+</template>--------------------------------
+{{pointsCost}} - {{unitCount}}/{{Math.ceil(unitCount / 2)}}</pre>
   </main>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import store from '@/store';
+
 export default {
-  name: 'TextView'
+  name: 'TextView',
+  computed: Object.assign({
+      units: () => Object.keys(store.getters.units)
+        .filter((id) => store.getters.units[id].number > 0)
+          .map((id) => Object.assign({ id: id }, store.getters.units[id])),
+    },
+    mapGetters(['armyList', 'label', 'pointsCost', 'unitCount', 'version'])
+  ),
+  methods: {
+    padLeft (str) {
+      var pointsCostLength = ('' + this.pointsCost).length;
+
+      str = '' + str;
+
+      while (str.length < pointsCostLength) {
+        str = ' ' + str;
+      }
+
+      return str;
+    },
+    unitUpgrades (unitID) {
+      return Object.keys(store.getters.units[unitID].upgrades || {})
+        .filter((id) => store.getters.units[unitID].upgrades[id].number > 0)
+          .map((id) => Object.assign({ id: id }, store.getters.units[unitID].upgrades[id]));
+    }
+  }
 };
 </script>
 
 <style lang="scss">
+  .text-view {
+    pre {
+      margin: 0;
+    }
+  }
 </style>
--- tests/unit/views/Text.spec.js
+++ tests/unit/views/Text.spec.js
@@ -1,15 +1,71 @@
-import { shallowMount } from '@vue/test-utils'
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
 
 import Text from '@/views/Text';
 
-var wrapper;
+const localVue = createLocalVue();
+
+var store, wrapper;
+
+jest.mock('@/store', () => {
+  return {
+    getters: {
+      units: {
+        a: {
+          number: 1,
+          pointsCost: 50,
+          upgrades: {
+            c: {
+              number: 1,
+              pointsCost: '+5'
+            },
+            d: {
+              number: 0,
+              pointsCost: '+10'
+            }
+          }
+        },
+        b: {
+          number: 0,
+          pointsCost: 0
+        },
+      }
+    }
+  }
+});
+
+localVue.use(Vuex);
 
 describe('Text.vue', () => {
   beforeEach(() => {
-    wrapper = shallowMount(Text, { });
+    store = new Vuex.Store({
+      getters: {
+        armyList: () => 'an army list',
+        label: () => 'a label',
+        pointsCost: () => 50,
+        unitCount: () => 1,
+        version: () => 'a version'
+      }
+    });
+
+    wrapper = shallowMount(Text, { localVue, store });
   });
 
   it('renders', () => {
-    expect(wrapper.text()).toEqual('Text');
+    expect(wrapper.text()).toContain('a label');
+    expect(wrapper.text()).toContain('an army list');
+    expect(wrapper.text()).toContain('50');
+    expect(wrapper.text()).toContain('a version');
+    expect(wrapper.text()).toContain('1/1');
+  });
+
+  it('only shows units with a positive number', () => {
+    expect(wrapper.text()).toContain('50 - 1 a');
+    expect(wrapper.text()).not.toContain('0 - 0 b');
+  });
+
+  it('only shows upgrades with a positive number', () => {
+    expect(wrapper.text()).toContain('   - 1 c (+5)');
+    expect(wrapper.text()).not.toContain('0 d (+10)');
   });
 });
