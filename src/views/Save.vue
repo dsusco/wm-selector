8661557eb71eed76df35afc6269110006c4895e1
David Susco <dsusco@clarku.edu>
2019-06-25 08:01:01 -0400

    added Save url method and tests

--- src/views/Save.vue
+++ src/views/Save.vue
@@ -1,12 +1,36 @@
 <template>
   <main id="main" class="save-view">
-    Save
+    <p>The following URL can be used to restore the current list:</p>
+
+    <textarea readonly v-html="url()" @focus="$event.target.select()"></textarea>
   </main>
 </template>
 
 <script>
+import Querystring from 'querystring';
+
+import store from '@/store';
+
 export default {
-  name: 'SaveView'
+  name: 'SaveView',
+  methods: {
+    url: () => window.location.origin + '/#/?' + Querystring.stringify(Object.keys(store.getters.usedUnits)
+      .reduce((url, unitID) => {
+        var unit = store.getters.usedUnits[unitID];
+
+        url[unit.order] = unit.number;
+
+        for (var upgradeID in unit.upgrades) {
+          url[unit.order + '-' + store.getters.upgrades[upgradeID].order] = unit.upgrades[upgradeID].number;
+        }
+
+        return url;
+      }, {
+        jsonPath: store.getters.jsonPath,
+        label: store.getters.label,
+        printItems: store.getters.printItems.join(',')
+      }))
+  }
 };
 </script>
 
--- tests/unit/views/Save.spec.js
+++ tests/unit/views/Save.spec.js
@@ -1,8 +1,32 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils';
 
 import Save from '@/views/Save';
 
 var wrapper;
+
+jest.mock('@/store', () => ({
+    getters: {
+      jsonPath: 'a jsonPath',
+      label: 'a label',
+      printItems: ['pi1', 'pi2'],
+      upgrades: {
+        'an upgrade': {
+          order: 'au'
+        }
+      },
+      usedUnits: {
+        'a unit': {
+          order: 0,
+          number: 2,
+          upgrades: {
+            'an upgrade': {
+              number: 1
+            }
+          }
+        }
+      }
+    }
+  }));
 
 describe('Save.vue', () => {
   beforeEach(() => {
@@ -10,6 +34,10 @@ describe('Save.vue', () => {
   });
 
   it('renders', () => {
-    expect(wrapper.text()).toEqual('Save');
+    expect(wrapper.find('textarea').text()).toContain('jsonPath=a%20jsonPath');
+    expect(wrapper.find('textarea').text()).toContain('label=a%20label');
+    expect(wrapper.find('textarea').text()).toContain('printItems=pi1%2Cpi2');
+    expect(wrapper.find('textarea').text()).toContain('0=2');
+    expect(wrapper.find('textarea').text()).toContain('0-au=1');
   });
 });
