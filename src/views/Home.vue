76f93a756a759840f51307c4fe84866295dbca1b
David Susco <dsusco@clarku.edu>
2024-11-06 13:29:18 -0500

    updated WMR pdfs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,8 +21,8 @@
     </ArmyListAccordion>
 
     <ArmyListAccordion :title="'Warmaster Revolution'">
-      <a target="_blank" title="Warmaster Revolution Rules" href="/pdfs/WMR 2.0.pdf"><span>Warmaster Revolution Rules</span></a>
-      <a target="_blank" title="Warmaster Revolution Armies" href="/pdfs/WMR_Armies2.23.pdf"><span>Warmaster Revolution Armies</span></a>
+      <a target="_blank" title="Warmaster Revolution Rules" href="/pdfs/WMR 2.0.1.pdf"><span>Warmaster Revolution Rules</span></a>
+      <a target="_blank" title="Warmaster Revolution Armies" href="/pdfs/WMR_Armies2.24.pdf"><span>Warmaster Revolution Armies</span></a>
     </ArmyListAccordion>
 
     <ArmyListAccordion :title="'Warmaster Ancients'">
